23bf80818470d62057954026bacddc8fe7261055
Daniel Amarante <daniel.amarante2@gmail.com>
2015-06-03 21:19:53 -0300

    poiefejrjr

--- PigProblem.java
+++ PigProblem.java
@@ -24,31 +24,41 @@ public class PigProblem {
 	}
 	
 	public int getAnswer() {
-		solve(p,g);
+		solve(p,g,0);
 		return answer;
 	}
 	
-	public void solve(int p, int g) {
+	public void solve(int p, int g, int pos) {
 		if(p == 0 && g == 0) {
-			printaMatriz();
+			// printaMatriz();
 			answer++;
 			return;
 		}
-		for (int i = 0; i < l; i++) {
-			for (int j = 0; j < l; j++) {
-				if(podePorco(i, j, p)) {
-					board[i][j] = PORCO;
-					solve(p-1,g);
-					board[i][j] = VAGO;
-				} else if(podeGalinha(i, j, g)) {
-					board[i][j] = GALINHA;
-					solve(p,g-1);
-					board[i][j] = VAGO;
-				}
+        if (p == 0 && g == this.g) {
+        	// if (!daPraBotarGalinhaNoNegocio()) {
+        	// 	return;
+        	// }
+        	pos = 0;
+        }
+        for(int n = pos; n < l * l; n++) {
+        	int i = n / l;
+        	int j = n % l;
+        	if(podePorco(i, j, p)) {
+        		int newpos = i * l + j;
+				board[i][j] = PORCO;
+				solve(p-1,g, newpos);
+				board[i][j] = VAGO;
+			} else if(podeGalinha(i, j, g)) {
+				int newpos = i * l + j;
+				board[i][j] = GALINHA;
+				solve(p,g-1,newpos);
+				board[i][j] = VAGO;
 			}
-		}
+
+        }
+        return;
 	}
-	
+
 	private boolean podePorco(int i, int j, int p) {
 		if(p == 0) 
 			return false;
@@ -67,7 +77,41 @@ public class PigProblem {
 				return false;
 			}
 		}
+		for(int n = i, m = j; n >= 0 && m >= 0; n--, m--) {
+			if(board[n][m] == PORCO)
+				return false; 
+		}
+		for(int n = i, m = j; n < l && m >= 0; n++, m--) {
+			if(board[n][m] == PORCO)
+				return false; 
+		}
+		for(int n = i, m = j; n >= 0 && m < l; n--, m++) {
+			if(board[n][m] == PORCO)
+				return false; 
+		}
+		for(int n = i, m = j; n < l && m < l; n++, m++) {
+			if(board[n][m] == PORCO)
+				return false; 
+		}
+		
 		return true;
+	}
+
+	private boolean daPraBotarGalinhaNoNegocio() {
+		int contador = 0;
+		for (int i = 0; i < l; i ++) {
+			for (int j = 0; j < l; j++) {
+				if (podeGalinha(i,j,400)) {
+					contador++;
+				}
+
+			}
+		}
+		if (contador < this.g) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 	
 		
@@ -83,16 +127,20 @@ public class PigProblem {
 	}
 	
 	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String oi = scan.nextLine();
-        String[] numbersString = oi.split(" ");
-        int[] numbers = new int[numbersString.length];
-        for (int i = 0; i < numbersString.length; i++) {
-            numbers[i] = Integer.parseInt(numbersString[i]);
-        }
+ //        Scanner scan = new Scanner(System.in);
+ //      //  while(true) {
+ //        	String oi = scan.nextLine();
+ //        String[] numbersString = oi.split(" ");
+ //        int[] numbers = new int[numbersString.length];
+ //        for (int i = 0; i < numbersString.length; i++) {
+ //            numbers[i] = Integer.parseInt(numbersString[i]);
+ //        }
         
-        PigProblem prob = new PigProblem(numbers[0],numbers[1],numbers[2]);
+ //        PigProblem prob = new PigProblem(numbers[0],numbers[1],numbers[2]);
+		PigProblem prob = new PigProblem(7,4,3);
 		System.out.println(prob.getAnswer());
+
+        //}
 
 	}
 	
